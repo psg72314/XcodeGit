a633fc0370a186f418608522bbf973a69ae0ddad
SCpeng <consonpeng@gmail.com>
2017-12-02 06:55:52 +0800

    branch commit4

--- XcodeGit/ViewController.swift
+++ XcodeGit/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        NSLog("commit4");
     }
 
     override func didReceiveMemoryWarning() {
