af9286a12eb94749eba59772df3268871b50c0c2
SCpeng <consonpeng@gmail.com>
2017-12-02 08:13:07 +0800

    commit9

--- XcodeGit/ViewController.swift
+++ XcodeGit/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController {
     }
 
     func test(){
-        NSLog("commit8");
+        NSLog("commit9");
         NSLog("commit3");
         NSLog("commit2");
     }
